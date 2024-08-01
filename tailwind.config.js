e45db12eea6ea7d34b2cb4ab7d91a8063486d304
rifqi011 <rifqibanusafingi11@gmail.com>
2024-08-01 08:35:28 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./public/index.php"],
+	content: ["./public/**/*.{html, php}"],
 	theme: {
 		extend: {
 			fontFamily: {
